f4a4a029f7ca87ec584a67ebfc02fccd84340abf
kinoshita daiki <kinoshitadaiki0517@gmail.com>
2024-02-18 22:21:08 +0900

    テストクラス作成

--- src/main/java/work/my/portfolio/service/ShortnerClientService.java
+++ src/main/java/work/my/portfolio/service/ShortnerClientService.java
@@ -2,6 +2,7 @@ package work.my.portfolio.service;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -10,6 +11,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import work.my.portfolio.entity.Shortner;
 
+@ApplicationScoped
 @Path("/")
 @RegisterRestClient
 public interface ShortnerClientService {
