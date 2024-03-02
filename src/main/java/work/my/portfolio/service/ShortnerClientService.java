2aab20038c23e785e16ab9490a0ba5717f01e657
kinoshita daiki <kinoshitadaiki0517@gmail.com>
2024-03-02 19:13:21 +0900

    JavaDocコメント追加

--- src/main/java/work/my/portfolio/service/ShortnerClientService.java
+++ src/main/java/work/my/portfolio/service/ShortnerClientService.java
@@ -11,21 +11,33 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import work.my.portfolio.entity.Shortner;
 
+/**
+ * 短縮機Service(クライアント側)
+ * 
+ * @author kinoshita daiki
+ * @since 2024/03/02
+ */
 @ApplicationScoped
 @Path("/")
 @RegisterRestClient
 public interface ShortnerClientService {
 
+	/**
+	 * 指定した長いURLに紐づいた短縮機を取得する
+	 * 
+	 * @param longUrl 長いURL
+	 * @return 短縮機
+	 */
 	@Path("urlShortnerGetter")
 	@Produces(MediaType.APPLICATION_JSON)
 	@POST
 	public Shortner getShortnerByLongUrl(String longUrl);
 
 	/**
-	 * 指定した短いURLに紐づいた長いURLでリダイレクトする
+	 * 指定した短いURLに紐づいた短縮機を取得する
 	 * 
 	 * @param shortUrl 短いURL
-	 * @return リダイレクトレスポンス
+	 * @return 短縮機
 	 */
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("{shortUrlPath}")
